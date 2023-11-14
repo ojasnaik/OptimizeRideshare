0083e25ba00e02cef4d231e6de32cacab3a44699
Ojas Ankush Naik <naikojas@gmail.com>
2023-11-14 13:01:08 -0800

    Initialized a SpringBoot project

---
+++ src/main/java/com/daa/optimizeRideshare/OptimizeRideshareApplication.java
@@ -0,0 +1,13 @@
+package com.daa.optimizeRideshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizeRideshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimizeRideshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daa/optimizeRideshare/OptimizeRideshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daa.optimizeRideshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizeRideshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
